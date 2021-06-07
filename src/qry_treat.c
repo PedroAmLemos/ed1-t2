33ab2fced7615e377d0d003c5ef0a1bb9e1d9b78
PedroAmlemos <peedroamlemos@gmail.com>
2021-06-07 17:55:26 -0300

    some renames

--- src/qry_treat.c
+++ src/qry_treat.c
@@ -8,8 +8,8 @@
 #include "void_pointers.h"
 #include "svg.h"
 
-bool intInter(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2){
-    return x1>=x2&&y1>=y2&&x1+w1<=x2+w2&&y1+h1<=y2+h2;
+bool point_is_inside(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2) {
+  return x1 >= x2 && y1 >= y2 && x1 + w1 <= x2 + w2 && y1 + h1 <= y2 + h2;
 }
 
 _Tree dpi(_Tree rect_tree, float x, float y, FILE *txtFile){
@@ -19,13 +19,13 @@ _Tree dpi(_Tree rect_tree, float x, float y, FILE *txtFile){
 	dpi(get_kd_right(rect_tree), x, y, txtFile);
 	dpi(get_kd_left(rect_tree), x, y, txtFile);
 	_Rect rectangle = get_info(rect_tree);
-	float rectX, rectY, rectW, rectH;
+	float rectW, rectH;
 	float *point = get_point(rect_tree);
 	rectW = get_rect_w(rectangle);
 	rectH = get_rect_h(rectangle);
-	if(intInter(x,y, 0, 0, point[0], point[1], rectW, rectH)){
-		fprintf(txtFile, "%s", get_rect_id(rectangle));
-		return delete_node_init(rect_tree, point);
+	if (point_is_inside(x, y, 0, 0, point[0], point[1], rectW, rectH)) {
+	  fprintf(txtFile, "%s", get_rect_id(rectangle));
+	  return delete_node_init(rect_tree, point);
 	}
 	return rect_tree;
 }
--- src/qry_treat.h
+++ src/qry_treat.h
@@ -3,8 +3,6 @@
 #ifndef __QRY__
 #define __QRY__ 
 
-_Tree dpi(_Tree rect_tree, float x, float y, FILE *txtFile);
-
 _Tree main_qry(_Tree rect_tree, _Tree circle_tree, FILE *qryFile, FILE *txtFile, FILE *svgFile);
 
 #endif /* ifndef __QRY__ */
