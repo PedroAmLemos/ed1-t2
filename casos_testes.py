3aced532267b58217458fd58d0f639a01669b111
PedroAmlemos <peedroamlemos@gmail.com>
2021-06-11 01:41:58 -0300

    some memory issues

--- casos_testes.py
+++ casos_testes.py
@@ -1,6 +1,6 @@
 from os import system
 from glob import glob
 # tests = glob("/home/pedro/Documents/t2-ed/t2/*.geo")
-qry = glob("/home/pedro/Documents/t2-ed/t2/aldeia-alguns-abrigados-grande/*.qry")
+qry = glob("/home/pedro/Documents/t2-ed/t2/aldeia-protegida-alguns-abrigados-grande/*.qry")
 for item in qry:
-    system(f'./src/progr -f /home/pedro/Documents/t2-ed/t2/hino.geo -o /home/pedro/Documents/t2-ed/output/ -q {item}')
+    system(f'valgrind ./src/progr -f /home/pedro/Documents/t2-ed/t2/aldeia-protegida-alguns-abrigados-grande.geo -o /home/pedro/Documents/t2-ed/output/ -q {item}')
