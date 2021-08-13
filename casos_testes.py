24554e305555be16ec8f0808a639351db5772734
PedroAmLemos <peedroamlemos@gmail.com>
2021-08-13 18:17:55 -0300

    added comment

--- casos_testes.py
+++ casos_testes.py
@@ -1,6 +1,7 @@
 from os import system
 from glob import glob
 # tests = glob("/home/pedro/Documents/t2-ed/t2/*.geo")
+# comentario
 qry = glob("/home/pedro/Documents/t2-ed/t2/hino/*.qry")
 for item in qry:
     system(f'./src/progr -f /home/pedro/Documents/t2-ed/t2/hino.geo -o /home/pedro/Documents/t2-ed/output/ -q {item}')
